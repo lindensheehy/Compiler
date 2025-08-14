886537b5a38514c1a862f96979ae4bf1f97bf2ed
Linden <linden.sheehy@outlook.com>
2025-08-14 16:13:50 -0400

    Finish sketching out disassembler

--- src/disassembler/disassembler.cpp
+++ src/disassembler/disassembler.cpp
@@ -1,115 +1,267 @@
+#include <string.h>
+#include <cstdint>
+#include <cstdio>
 
-const char* opcodeMap[] = {
-    "add" // 0x00
-};
+size_t writeOpcode(char opcode, char* writeBuffer, size_t* writeBufferLength) {
 
+    size_t strLength;
+    const char* str = getOpcode(static_cast<uint8_t>(opcode), &strLength);
 
-void writeOpcode(char opcode, char* writeBuffer, int* writeBufferLength) {
+    char* writePtr = writeBuffer + (*writeBufferLength);
+    memcpy(writePtr, str, strLength);
 
-    switch (opcode) {
+    (*writeBufferLength) += strLength;
 
-        case 0x00: {
-            writeBuffer[(*writeBufferLength)]     = 'a';
-            writeBuffer[(*writeBufferLength) + 1] = 'd';
-            writeBuffer[(*writeBufferLength) + 2] = 'd';
-            writeBuffer[(*writeBufferLength) + 3] = ' ';
-            (*writeBufferLength) += 4;
-            break;
-        }
+    return 1;
 
-        case 0x01: {
-            writeBuffer[(*writeBufferLength)]     = 's';
-            writeBuffer[(*writeBufferLength) + 1] = 'u';
-            writeBuffer[(*writeBufferLength) + 2] = 'b';
-            writeBuffer[(*writeBufferLength) + 3] = ' ';
-            (*writeBufferLength) += 4;
-            break;
-        }
+}
 
-        // etc
+size_t writeRegister(char reg, char* writeBuffer, size_t* writeBufferLength) {
+    
+    size_t strLength;
+    const char* str = getRegister(static_cast<uint8_t>(reg), &strLength);
+
+    char* writePtr = writeBuffer + (*writeBufferLength);
+    memcpy(writePtr, str, strLength);
+
+    (*writeBufferLength) += strLength;
+
+    return 1;
+
+}
+
+size_t writeMemory(char* fileData, size_t startIndex, char* writeBuffer, size_t* writeBufferLength) {
+    
+    writeBuffer[(*writeBufferLength)] = '[';
+    (*writeBufferLength)++;
+
+    writeRegister(fileData[startIndex], writeBuffer, writeBufferLength);
+
+    char offsetLength = fileData[startIndex + 1];
+    char offsetFirst = fileData[startIndex + 2];
+    if (offsetLength != '1' || offsetFirst != 0x00) {
+
+        static constexpr char padding[] = " + ";
+        constexpr size_t paddingLen = sizeof(padding) - 1;
+
+        memcpy(writeBuffer + (*writeBufferLength), padding, paddingLen);
+        (*writeBufferLength) += paddingLen;
+
+        writeImmediate(fileData, startIndex + 1, writeBuffer, writeBufferLength);
 
     }
 
-    return;
+    writeBuffer[(*writeBufferLength)] = ']';
+    (*writeBufferLength)++;
+
+    size_t consumed = 2;
+    switch (offsetLength) {
+        case '1':
+            consumed += 1;
+            break;
+        case '4':
+            consumed += 4;
+            break;
+        default:
+            // log error case
+            break;
+    }
+
+    return consumed;
 
 }
 
-void writeRegister(char reg, char* writeBuffer, int* writeBufferLength) {
-    return;
+size_t writeImmediate(char* fileData, size_t startIndex, char* writeBuffer, size_t* writeBufferLength) {
+
+    size_t deltaIndex;
+    int written;
+
+    switch (fileData[startIndex]) {
+
+        case '1': {
+
+            uint8_t value = fileData[startIndex + 1];
+
+            const size_t writeSize = 5; // ex. "0x12\0"
+            written = std::snprintf(writeBuffer, writeSize, "0x%X", value);
+
+            deltaIndex = 2; // '1' and the byte value
+
+            break;
+
+        }
+
+        case '4': {
+
+            uint32_t value;
+            memcpy(&value, fileData + startIndex + 1, sizeof(uint32_t));
+
+            const size_t writeSize = 11; // ex. "0x12345678\0"
+            written = std::snprintf(writeBuffer, writeSize, "0x%X", value);
+
+            deltaIndex = 5; // '4' and the 4 byte value
+
+            break;
+
+        }
+
+        default: {
+            // log error case
+            break;
+        }
+
+    }
+
+    (*writeBufferLength) += written;
+
+    return deltaIndex;
+
 }
 
-void writeMemory(int startIndex, char* writeBuffer, int* writeBufferLength) {
-    return;
-}
+void writePadding(char lastPrefix, char nextPrefix, char* writeBuffer, size_t* writeBufferLength) {
 
-void writeIntermediate(int startIndex, char* writeBuffer, int* writeBufferLength) {
+    /*
+        There are three types of padding:
+        1. A newline "\r\n". This goes before opcodes
+        2. A single space ' '. This goes after opcodes
+        3. A comma and space ", ". This goes between operands (when the above two cases dont apply)
+    */
+
+    static constexpr char newLinePadding[] = "\r\n";
+    constexpr size_t newLinePaddingLen = sizeof(newLinePadding) - 1;
+
+    static constexpr char spacePadding[] = " ";
+    constexpr size_t spacePaddingLen = sizeof(spacePadding) - 1;
+
+    static constexpr char commaSpacePadding[] = ", ";
+    constexpr size_t commaSpacePaddingLen = sizeof(commaSpacePadding) - 1;
+
+    struct {
+        const char* s;
+        size_t len;
+    } padding;
+
+    // Case 1
+    if (nextPrefix == 'O') {
+        padding.s = newLinePadding;
+        padding.len = newLinePaddingLen;
+    }
+
+    // Case 2
+    else if (lastPrefix == 'O') {
+        padding.s = spacePadding;
+        padding.len = spacePaddingLen;
+    }
+
+    // Case 3
+    else {
+        padding.s = commaSpacePadding;
+        padding.len = commaSpacePaddingLen;
+    }
+
+    char* writeBufferPointer = writeBuffer + (*writeBufferLength);
+    memcpy(writeBufferPointer, padding.s, padding.len);
+
+    (*writeBufferLength) += padding.len;
+
     return;
+    
 }
 
 void generateDisassemble(const char* fileNameIn, const char* fileNameOut) {
 
     char* file = readFile(fileNameIn);
-    int fileLength = getFileLength(fileNameIn);
+    size_t fileLength = getFileLength(fileNameIn);
 
-    constexpr int bufferSize = 2048;
-    char* writeBuffer = new char[bufferSize];
-    int writeBufferLength = 0;
-    const int writeBufferLimit = bufferSize - 64;
+    constexpr size_t WRITE_BUFFER_SIZE = 65536;
+    char* writeBuffer = new char[WRITE_BUFFER_SIZE] {0x00};
+    size_t writeBufferLength = 0;
+    constexpr size_t WRITE_BUFFER_LIMIT = WRITE_BUFFER_SIZE - 64;
 
-    for (int i = 0; i < fileLength; i++) {
+    char lastPrefix = 0;
+    size_t bytesConsumed;
+
+    {
+        // First, write the file name as a comment at the top
+        static constexpr char fileNamePrefix[] = "; File: ";
+        constexpr size_t fileNamePrefixLen = sizeof(fileNamePrefix) - 1;
+
+        memcpy(writeBuffer, fileNamePrefix, fileNamePrefixLen);
+        writeBufferLength += fileNamePrefixLen;
+
+        size_t fileNameInLen = strlen(fileNameIn);
+        memcpy(writeBuffer + writeBufferLength, fileNameIn, fileNameInLen);
+        writeBufferLength += fileNameInLen;
+    }
+
+    initMaps();
+
+    for (size_t i = 0; i < fileLength; i++) {
+
+        writePadding(lastPrefix, file[i], writeBuffer, &writeBufferLength);
 
         switch (file[i]) {
-
+            
+            // Opcode
             case 'O': {
 
-                writeOpcode(file[i + 1], writeBuffer, &writeBufferLength);
-                i += 1;
+                bytesConsumed = writeOpcode(file[i + 1], writeBuffer, &writeBufferLength);
+                i += bytesConsumed;
 
                 break;
 
             }
 
+            // Register
             case 'R': {
 
-                writeRegister(file[i + 1], writeBuffer, &writeBufferLength);
-                i += 1;
+                bytesConsumed = writeRegister(file[i + 1], writeBuffer, &writeBufferLength);
+                i += bytesConsumed;
                 
                 break;
                 
             }
 
+            // Memory
             case 'M': {
 
-                writeMemory(i + 1, writeBuffer, &writeBufferLength);
-                i += 1; // this would need to be dynamic
+                bytesConsumed = writeMemory(file, i + 1, writeBuffer, &writeBufferLength);
+                i += bytesConsumed;
                 
                 break;
                 
             }
 
+            // Immediate
             case 'I': {
 
-                writeIntermediate(i + 1, writeBuffer, &writeBufferLength);
-                i += 1; // this would need to be dynamic
+                bytesConsumed = writeImmediate(file, i + 1, writeBuffer, &writeBufferLength);
+                i += bytesConsumed;
                 
                 break;
                 
             }
 
             default: {
-                std::cout << "Failed to disassemble! Reason: Found invalid token prefix char(" << int(file[i]) << ")" << std::endl;
+                // log error case
                 return;
             }
 
         }
 
-        if (writeBufferLength > writeBufferLimit) {
+        if (writeBufferLength > WRITE_BUFFER_LIMIT) {
             writeFile(fileNameOut, writeBuffer, writeBufferLength);
-            memset(writeBuffer, 0x00, bufferSize);
+            memset(writeBuffer, 0x00, WRITE_BUFFER_SIZE);
             writeBufferLength = 0;
         }
 
+        lastPrefix = file[i];
+
     }
+
+    writeFile(fileNameOut, writeBuffer, writeBufferLength);
+    delete[] writeBuffer;
+    delete[] file;
 
     return;
 
