87b9317d4a5e440dd46fe21015b6d1bdf4251869
DR22740 <vpopo092@uottawa.ca>
2025-08-31 02:43:50 -0400

    added the logic for getFileLength

--- src/core/fileio.cpp
+++ src/core/fileio.cpp
@@ -1,13 +1,22 @@
 #include "core/fileio.h"
-
-
-
-char* readFile(const char* fileName) {
-
+#include <cstdio>
+#include <filesystem>
+#include <system_error>
+char* readFile(const char* fileName)
+{
+    size_t file_length = getFileLength(fileName);
+        
 }
 
-size_t getFileLength(const char* fileName) {
-
+size_t getFileLength(const char* fileName) 
+{
+    std::error_code ec;
+    std::uintmax_t sz = std::filesystem::file_size(fileName, ec);
+    if(ec){
+        return 0;
+    }else{
+        return sz;
+    }
 }
 
 void writeFile(const char* fileName, const char* writeBuffer, size_t writeBufferLength) {
