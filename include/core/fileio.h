e4561df49924ada995afc40e422b9e739cc4e8d1
lindensheehy <linden.sheehy@outlook.com>
2025-09-04 11:03:28 -0400

    Improve fileio code

--- include/core/fileio.h
+++ include/core/fileio.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdint>
 
 
 // Reads from fileName and returns a new heap allocated buffer containing the contents
-char* readFile(const char* fileName);
+uint8_t* readFile(const char* fileName);
 
 // Returns the length of fileName
-bool getFileLength(const char* fileName, size_t * file_length);
+bool getFileLength(const char* fileName, uintmax_t* fileLengthOut);
 
 // Appends the contents of writeBuffer to fileNameOut. Will write exactly writeBufferLength bytes.
-void writeFile(const char* fileName, const char* writeBuffer, size_t writeBufferLength);
+void writeFile(const char* fileName, uint8_t* writeBuffer, size_t writeBufferLength);
--- src/core/fileio.cpp
+++ src/core/fileio.cpp
@@ -3,46 +3,38 @@
 #include <filesystem>
 #include <system_error>
 
+uint8_t* readFile(const char* fileName) {
 
-#define ELEMENT_SIZE 1
-char * readFile(const char* fileName)
-{
     size_t file_length;
     bool file_length_found = getFileLength(fileName, &file_length);
-    if(file_length_found)
-    {
-        return nullptr;
-    }
+    if (file_length_found) return nullptr;
     if (file_length == 0) return nullptr;
 
     std::FILE* file = std::fopen(fileName, "rb");
     if (!file) return nullptr;
-    char* buf = new char[file_length];
-    size_t off = 0;
-    while (off < file_length) 
-    {
-        size_t n = std::fread(buf + off, ELEMENT_SIZE, file_length - off, file);
-        if (n == 0) 
-        {
-            delete[] buf;
-            std::fclose(file);
-            return nullptr;
-        }
-        off += n;
-    }
+
+    uint8_t* buf = new uint8_t[file_length];
+    size_t n = std::fread(buf, file_length, 1, file);
     std::fclose(file);
+
+    if (n == 0) {
+        delete[] buf;
+        return nullptr;
+    }
+
     return buf;   
+
 }
 
-bool getFileLength(const char* fileName, std::uintmax_t * size) 
-{
+bool getFileLength(const char* fileName, std::uintmax_t * size) {
+
     std::error_code ec;
-    *size = std::filesystem::file_size(fileName, ec);
-    if(ec){
-        return false;
-    }else{
-        return true;
-    }
+    std::uintmax_t temp = std::filesystem::file_size(fileName, ec);
+    if (ec) return true;
+    
+    *size = temp;
+    return false;
+
 }
 
 void writeFile(const char* fileName, const char* writeBuffer, size_t writeBufferLength) {
