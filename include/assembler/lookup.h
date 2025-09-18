e8bdafa35c52f26f39f0b135e6ade1e138b5dd57
lindensheehy <linden.sheehy@outlook.com>
2025-09-18 16:55:57 -0400

    Add priority weighting to instruction lookup in assembler

--- include/assembler/lookup.h
+++ include/assembler/lookup.h
@@ -29,6 +29,7 @@ enum class SignatureOperandType : uint8_t {
 };
 
 struct InstructionSignature {
+    uint8_t preference;
     Opcode opcode;
     SignatureOperandType operands[3];
 };
@@ -36,128 +37,128 @@ struct InstructionSignature {
 constexpr InstructionSignature lookup[] = {
 
     // ADD
-    {Opcode::ADD,   {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
-    {Opcode::ADD,   {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
-    {Opcode::ADD,   {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
-    {Opcode::ADD,   {SignatureOperandType::RM32,      SignatureOperandType::IMM8,     SignatureOperandType::NONE}},
-    {Opcode::ADD,   {SignatureOperandType::EAX,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {2, Opcode::ADD,   {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
+    {2, Opcode::ADD,   {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
+    {4, Opcode::ADD,   {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {3, Opcode::ADD,   {SignatureOperandType::RM32,      SignatureOperandType::IMM8,     SignatureOperandType::NONE}},
+    {1, Opcode::ADD,   {SignatureOperandType::EAX,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
 
     // SUB
-    {Opcode::SUB,   {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
-    {Opcode::SUB,   {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
-    {Opcode::SUB,   {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
-    {Opcode::SUB,   {SignatureOperandType::RM32,      SignatureOperandType::IMM8,     SignatureOperandType::NONE}},
-    {Opcode::SUB,   {SignatureOperandType::EAX,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {2, Opcode::SUB,   {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
+    {2, Opcode::SUB,   {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
+    {4, Opcode::SUB,   {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {3, Opcode::SUB,   {SignatureOperandType::RM32,      SignatureOperandType::IMM8,     SignatureOperandType::NONE}},
+    {1, Opcode::SUB,   {SignatureOperandType::EAX,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
 
     // INC
-    {Opcode::INC,   {SignatureOperandType::R32,       SignatureOperandType::NONE,     SignatureOperandType::NONE}},
-    {Opcode::INC,   {SignatureOperandType::RM32,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::INC,   {SignatureOperandType::R32,       SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {2, Opcode::INC,   {SignatureOperandType::RM32,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // DEC
-    {Opcode::DEC,   {SignatureOperandType::R32,       SignatureOperandType::NONE,     SignatureOperandType::NONE}},
-    {Opcode::DEC,   {SignatureOperandType::RM32,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::DEC,   {SignatureOperandType::R32,       SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {2, Opcode::DEC,   {SignatureOperandType::RM32,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // IMUL
-    {Opcode::IMUL,  {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
-    {Opcode::IMUL,  {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::IMM32}},
-    {Opcode::IMUL,  {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::IMM8}},
-    {Opcode::IMUL,  {SignatureOperandType::RM32,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {4, Opcode::IMUL,  {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
+    {3, Opcode::IMUL,  {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::IMM32}},
+    {2, Opcode::IMUL,  {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::IMM8}},
+    {1, Opcode::IMUL,  {SignatureOperandType::RM32,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // IDIV
-    {Opcode::IDIV,  {SignatureOperandType::RM32,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::IDIV,  {SignatureOperandType::RM32,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // CDQ
-    {Opcode::CDQ,   {SignatureOperandType::NONE,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::CDQ,   {SignatureOperandType::NONE,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // AND
-    {Opcode::AND,   {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
-    {Opcode::AND,   {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
-    {Opcode::AND,   {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
-    {Opcode::AND,   {SignatureOperandType::RM32,      SignatureOperandType::IMM8,     SignatureOperandType::NONE}},
-    {Opcode::AND,   {SignatureOperandType::EAX,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {2, Opcode::AND,   {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
+    {2, Opcode::AND,   {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
+    {4, Opcode::AND,   {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {3, Opcode::AND,   {SignatureOperandType::RM32,      SignatureOperandType::IMM8,     SignatureOperandType::NONE}},
+    {1, Opcode::AND,   {SignatureOperandType::EAX,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
 
     // OR
-    {Opcode::OR,    {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
-    {Opcode::OR,    {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
-    {Opcode::OR,    {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
-    {Opcode::OR,    {SignatureOperandType::RM32,      SignatureOperandType::IMM8,     SignatureOperandType::NONE}},
-    {Opcode::OR,    {SignatureOperandType::EAX,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {2, Opcode::OR,    {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
+    {2, Opcode::OR,    {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
+    {4, Opcode::OR,    {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {3, Opcode::OR,    {SignatureOperandType::RM32,      SignatureOperandType::IMM8,     SignatureOperandType::NONE}},
+    {1, Opcode::OR,    {SignatureOperandType::EAX,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
 
     // XOR
-    {Opcode::XOR,   {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
-    {Opcode::XOR,   {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
-    {Opcode::XOR,   {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
-    {Opcode::XOR,   {SignatureOperandType::RM32,      SignatureOperandType::IMM8,     SignatureOperandType::NONE}},
-    {Opcode::XOR,   {SignatureOperandType::EAX,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {2, Opcode::XOR,   {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
+    {2, Opcode::XOR,   {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
+    {4, Opcode::XOR,   {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {3, Opcode::XOR,   {SignatureOperandType::RM32,      SignatureOperandType::IMM8,     SignatureOperandType::NONE}},
+    {1, Opcode::XOR,   {SignatureOperandType::EAX,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
 
     // CMP
-    {Opcode::CMP,   {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
-    {Opcode::CMP,   {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
-    {Opcode::CMP,   {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
-    {Opcode::CMP,   {SignatureOperandType::RM32,      SignatureOperandType::IMM8,     SignatureOperandType::NONE}},
-    {Opcode::CMP,   {SignatureOperandType::EAX,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {2, Opcode::CMP,   {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
+    {2, Opcode::CMP,   {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
+    {4, Opcode::CMP,   {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {3, Opcode::CMP,   {SignatureOperandType::RM32,      SignatureOperandType::IMM8,     SignatureOperandType::NONE}},
+    {1, Opcode::CMP,   {SignatureOperandType::EAX,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
 
     // TEST
-    {Opcode::TEST,  {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
-    {Opcode::TEST,  {SignatureOperandType::EAX,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
-    {Opcode::TEST,  {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {2, Opcode::TEST,  {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
+    {1, Opcode::TEST,  {SignatureOperandType::EAX,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {3, Opcode::TEST,  {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
 
     // JMP
-    {Opcode::JMP,   {SignatureOperandType::REL8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
-    {Opcode::JMP,   {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::JMP,   {SignatureOperandType::REL8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {2, Opcode::JMP,   {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // JE
-    {Opcode::JE,    {SignatureOperandType::REL8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
-    {Opcode::JE,    {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::JE,    {SignatureOperandType::REL8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {2, Opcode::JE,    {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // JNE
-    {Opcode::JNE,   {SignatureOperandType::REL8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
-    {Opcode::JNE,   {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::JNE,   {SignatureOperandType::REL8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {2, Opcode::JNE,   {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // JG
-    {Opcode::JG,    {SignatureOperandType::REL8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
-    {Opcode::JG,    {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::JG,    {SignatureOperandType::REL8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {2, Opcode::JG,    {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // JL
-    {Opcode::JL,    {SignatureOperandType::REL8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
-    {Opcode::JL,    {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::JL,    {SignatureOperandType::REL8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {2, Opcode::JL,    {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // JGE
-    {Opcode::JGE,   {SignatureOperandType::REL8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
-    {Opcode::JGE,   {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::JGE,   {SignatureOperandType::REL8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {2, Opcode::JGE,   {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // JLE
-    {Opcode::JLE,   {SignatureOperandType::REL8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
-    {Opcode::JLE,   {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::JLE,   {SignatureOperandType::REL8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {2, Opcode::JLE,   {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // CALL
-    {Opcode::CALL,  {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::CALL,  {SignatureOperandType::REL32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // RET
-    {Opcode::RET,   {SignatureOperandType::NONE,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
-    {Opcode::RET,   {SignatureOperandType::IMM16,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::RET,   {SignatureOperandType::NONE,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {2, Opcode::RET,   {SignatureOperandType::IMM16,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // MOV
-    {Opcode::MOV,   {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
-    {Opcode::MOV,   {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
-    {Opcode::MOV,   {SignatureOperandType::R32,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
-    {Opcode::MOV,   {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {2, Opcode::MOV,   {SignatureOperandType::RM32,      SignatureOperandType::R32,      SignatureOperandType::NONE}},
+    {2, Opcode::MOV,   {SignatureOperandType::R32,       SignatureOperandType::RM32,     SignatureOperandType::NONE}},
+    {1, Opcode::MOV,   {SignatureOperandType::R32,       SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
+    {3, Opcode::MOV,   {SignatureOperandType::RM32,      SignatureOperandType::IMM32,    SignatureOperandType::NONE}},
 
     // LEA
-    {Opcode::LEA,   {SignatureOperandType::R32,       SignatureOperandType::MEM,      SignatureOperandType::NONE}},
+    {1, Opcode::LEA,   {SignatureOperandType::R32,       SignatureOperandType::MEM,      SignatureOperandType::NONE}},
 
     // PUSH
-    {Opcode::PUSH,  {SignatureOperandType::R32,       SignatureOperandType::NONE,     SignatureOperandType::NONE}},
-    {Opcode::PUSH,  {SignatureOperandType::RM32,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
-    {Opcode::PUSH,  {SignatureOperandType::IMM8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
-    {Opcode::PUSH,  {SignatureOperandType::IMM32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {3, Opcode::PUSH,  {SignatureOperandType::R32,       SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {4, Opcode::PUSH,  {SignatureOperandType::RM32,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::PUSH,  {SignatureOperandType::IMM8,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {2, Opcode::PUSH,  {SignatureOperandType::IMM32,     SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // POP
-    {Opcode::POP,   {SignatureOperandType::R32,       SignatureOperandType::NONE,     SignatureOperandType::NONE}},
-    {Opcode::POP,   {SignatureOperandType::RM32,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::POP,   {SignatureOperandType::R32,       SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {2, Opcode::POP,   {SignatureOperandType::RM32,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
     // NOP
-    {Opcode::NOP,   {SignatureOperandType::NONE,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
+    {1, Opcode::NOP,   {SignatureOperandType::NONE,      SignatureOperandType::NONE,     SignatureOperandType::NONE}},
 
 };
 
